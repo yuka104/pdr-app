430ff46e8853f4244b2ea5bd81a5d288c6322a23
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-12-04 14:23:31 +0900

    ストロングパラメーター実装

--- app/controllers/preps_controller.rb
+++ app/controllers/preps_controller.rb
@@ -1,4 +1,20 @@
 class PrepsController < ApplicationController
+
   def index
+  end
+
+  def new
+    @prep = Prep.new
+  end
+
+  def create
+    @prep = Prep.new(prep_params)
+  end
+
+
+
+  private
+  def prep_params
+    params.require(:prep).permit(:purpose, :reason, :task, :memo).merge(user_id: current_user.id)
   end
 end
