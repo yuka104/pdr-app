c3accded7c45ed98c6a07791c766d07b585eb9f2
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-12-08 15:51:14 +0900

    バリデーションの追加と単体モデルテスト追加

--- spec/factories/preps.rb
+++ spec/factories/preps.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :prep do
     purpose {Faker::Lorem.sentence}
-    reason {Faker::Lorem.sentence}
-    task {Faker::Lorem.sentence}
+    reason  {Faker::Lorem.sentence}
+    task    {Faker::Lorem.sentence}
+    memo    {Faker::Lorem.sentence}
+
     association :user
     
   end
