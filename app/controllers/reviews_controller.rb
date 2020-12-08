fd056cbc9332066a15bf4e636c56316a9bb3d8c9
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-12-08 19:15:59 +0900

    REVIEW投稿画面実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,9 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
   end
 
+  def show
+  end
+
   private
   def review_params
     params.require(:review).permit(:result, :level_id, :success, :failure, :improve).merge(user_id: current_user.id, review_id: params[:review_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "preps#index"
   resources :preps do
-    resources :prep, only: [:index,:new, :create]
+    resources :reviews, only: [:new, :create, :show]
   end
 end
