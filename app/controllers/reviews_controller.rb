35f6b9d50e0a6abc5f4306453333bb4f44d91cde
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-12-08 18:15:05 +0900

    コントローラーと生成

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:result, :level_id, :success, :failure, :improve).merge(user_id: current_user.id, review_id: params[:review_id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "preps#index"
-  resources :preps
+  resources :preps do
+    resources :prep, only: [:index,:new, :create]
+  end
 end
