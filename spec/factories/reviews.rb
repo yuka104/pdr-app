6df7328571c0ccd59ed54be6a967d55c39ee469a
yuka104 <72967188+yuka104@users.noreply.github.com>
2020-12-09 22:05:19 +0900

    REVIEWモデルの単体テスト追加

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :review do
+    result        {Faker::Lorem.sentence}
+    level_id      {2}
+    success       {Faker::Lorem.sentence}
+    failure       {Faker::Lorem.sentence}
+    improve       {Faker::Lorem.sentence}
     
+    association :user
+    association :prep
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @review = FactoryBot.build(:review)
+  end
+
+  describe 'review新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "全項目が存在すれば登録できる" do
+        expect(@review).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it "resultが空では登録できない" do
+        @review.result = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Result can't be blank")
+      end
+      it "level_idが空では登録できない" do
+        @review.level_id = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Level can't be blank")
+      end
+      it "level_idで１番目を選ぶと登録できない" do
+        @review.level_id = 1
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Level must be other than 1")
+      end
+      it "successが空では登録できない" do
+        @review.success = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Success can't be blank")
+      end
+      it "failureが空では登録できない" do
+        @review.failure = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Failure can't be blank")
+      end
+      it "improveが空では登録できない" do
+        @review.improve = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Improve can't be blank")
+      end
+      
+    end
+  end
 end
